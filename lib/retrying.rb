77b8f6061eab8399ec35ace4dd7f94d0ce71210e
Tomasz Szymczyszyn <tomasz.szymczyszyn@getbase.com>
2013-10-17 09:50:00 +0200

    Add :sleep option.

--- lib/retrying.rb
+++ lib/retrying.rb
@@ -2,7 +2,7 @@ require 'retrying/version'
 
 module Retrying
   def retrying(options = {}, &block)
-    options = { :on => StandardError, :tries => 2 }.merge(options)
+    options = { :on => StandardError, :tries => 2, :sleep => 0 }.merge(options)
     exceptions = Array(options[:on])
 
     attempts = 0
@@ -11,6 +11,7 @@ module Retrying
     rescue *exceptions => ex
       attempts += 1
       raise if attempts >= options[:tries]
+      sleep(options[:sleep]) if options[:sleep] > 0
       retry
     end
   end
--- spec/retrying_spec.rb
+++ spec/retrying_spec.rb
@@ -2,6 +2,8 @@ require 'retrying'
 
 include Retrying
 
+TestError = Class.new(StandardError)
+
 describe Retrying do
   describe '#retrying' do
     it 'retries a block once with no args' do
@@ -11,9 +13,9 @@ describe Retrying do
       begin
         retrying do
           tries += 1
-          raise FloatDomainError
+          raise TestError
         end
-      rescue FloatDomainError
+      rescue TestError
         raised = true
       end
 
@@ -28,9 +30,9 @@ describe Retrying do
       begin
         retrying(:tries => 3) do
           tries += 1
-          raise StandardError
+          raise TestError
         end
-      rescue StandardError
+      rescue TestError
         raised = true
       end
 
@@ -45,14 +47,29 @@ describe Retrying do
       begin
         retrying(:on => IOError) do
           tries += 1
-          raise FloatDomainError
+          raise TestError
         end
-      rescue FloatDomainError
+      rescue TestError
         raised = true
       end
 
       expect(tries).to eq(1)
       expect(raised).to be_true
+    end
+
+    it 'sleeps before retrying' do
+      def sleep(time)
+        @slept = time
+      end
+
+      begin
+        retrying(:sleep => 1) do
+          raise TestError
+        end
+      rescue TestError
+      end
+
+      expect(@slept).to eq(1)
     end
   end
 end
